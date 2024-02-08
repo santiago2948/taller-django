092623ef2556ba37518faecc2c3a4f2c59576268
Santiago <rasua2948@gmail.com>
2024-02-08 11:00:43 -0500

    nuevo arcgivo about.html

--- movie/views.py
+++ movie/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {"name":"Greg Lim"})
+    return render(request, 'home.html', {"name":"paola vallejo"})
    
