a201d88ef83b6c8c243fdfdd7ef42379d0e2451c
Santiago <rasua2948@gmail.com>
2024-02-08 16:22:10 -0500

    parte de clase terminada

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {"name":"paola vallejo"})
+    searchTerm= request.GET.get("searchMovie")
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+        pass
+    return render(request, 'home.html', {"searchTerm":searchTerm, "movies": movies, "name":"santiago acevedo"})
 
 def about(request):
     return render(request, "about.html", {"name":"santiago acevedo"})
